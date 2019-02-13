16f75aa9d4c2cfbcae616ec6c5176ff67b9e8a2b
Julius Stopforth <jjstopforth@gmail.com>
2019-02-13 19:27:01 +0200

    Add ListView and TaskArrayAdapter.
    The commit swaps out the fiddly view system that was previously used for a much nicer `ListView`. This required extending `ArrayAdapter` into `TaskArrayAdapter` that uses the `taskMutableList` in the main activity. Some layout formatting should still be done to make it look nicer.

--- app/src/main/java/com/sudosays/torro/MainActivity.kt
+++ app/src/main/java/com/sudosays/torro/MainActivity.kt
@@ -1,65 +1,46 @@
 package com.sudosays.torro
 
-import android.app.ActionBar
+
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val taskMutableList = mutableListOf<Task>()
+    lateinit var listViewAdapter: TaskArrayAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        val t = Task("Say Hello", 1.0f)
+        taskMutableList.add(Task("Say Hello", 1.0f))
+        taskMutableList.add(Task("Hello World", 1.0f))
 
-        singleTask.populate(t)
+        taskMutableList.add(Task("Do something else", 1.0f))
 
+
+        listViewAdapter = TaskArrayAdapter(this, R.layout.view_task, taskMutableList)
+        taskListView.adapter = listViewAdapter
     }
 
     fun addTask(view: View){
 
-        val intent:Intent = Intent(this, AddTask::class.java)
+        val intent = Intent(this, AddTask::class.java)
         startActivity(intent)
 
     }
 
     fun startFlow(view: View) {
-        val intent: Intent = Intent(this, FlowOverview::class.java)
+        val intent = Intent(this, FlowOverview::class.java)
         startActivity(intent)
     }
 
-    fun changeTaskInfo(view: View) {
-
-        val t = Task("This has changed", 0.75f)
-
-        //singleTask.populate(t)
-
-        val testView = TaskView(this)
-
-        testView.id = 1
-
-        container.addView(testView)
-
-        var constraintSet = ConstraintSet()
-
-        constraintSet.clone(container)
-
-        constraintSet.connect(testView.id, ConstraintSet.TOP, singleTask.id, ConstraintSet.BOTTOM,8)
-        constraintSet.connect(testView.id, ConstraintSet.START, container.id, ConstraintSet.START,16)
-        constraintSet.connect(testView.id, ConstraintSet.END, container.id, ConstraintSet.END,16)
-        constraintSet.setTranslationZ(testView.id,2.0f)
-        constraintSet.applyTo(container)
-
-        testView.populate(t)
-
-        Toast.makeText(this,"Created a new task view!",Toast.LENGTH_SHORT).show()
-
-    }
 
 }
---
+++ app/src/main/java/com/sudosays/torro/TaskArrayAdapter.kt
@@ -0,0 +1,29 @@
+package com.sudosays.torro
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+
+/**
+ *
+ * Author: Julius Stopforth
+ *
+ * Created on: 2019/02/13
+ *
+ */
+class TaskArrayAdapter constructor(
+        context: Context,
+        resource: Int,
+        objects: MutableList<Task>
+        ): ArrayAdapter<Task>(context, resource, objects)
+{
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val taskView = TaskView(parent!!.context)
+        taskView.populate(getItem(position))
+        return taskView
+
+        //return super.getView(position, convertView, parent)
+    }
+}
