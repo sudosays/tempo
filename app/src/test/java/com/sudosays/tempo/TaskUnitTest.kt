9dc98c49570b98539beb9be79ac727da553815b9
Julius Stopforth <jjstopforth@gmail.com>
2020-06-01 11:18:50 +0200

    Add basic unit testing with Truth lib
    This is a test of a data class (which may not require unit testing?) and
    adds the Truth assertion lib to the build.gradle file. This is the first
    step in addressing the technical debt discussed in issue #8
    (https://github.com/sudosays/tempo/issues/8#issue-628244221)

--- app/src/test/java/com/sudosays/tempo/TaskUnitTest.kt
+++ app/src/test/java/com/sudosays/tempo/TaskUnitTest.kt
@@ -1,5 +1,20 @@
 package com.sudosays.tempo
 
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import com.sudosays.tempo.data.Task
+
 /**
  * Created by julius on 2020/06/01.
  */
+class TaskUnitTest {
+
+    @Test
+    fun instantiationValidationCorrectSimpleReturnsTrue() {
+
+        val tempTask = Task(0,"test", 0,0)
+
+        assertThat(tempTask.name).isEqualTo("test")
+
+    }
+}
